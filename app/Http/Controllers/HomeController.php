b4000a81c55e44b5a1683c796fcc6b169752df6e
diogolimas <diogo.libras43@gmail.com>
2019-07-19 08:56:53 -0300

    início da listagem dos postagens

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class HomeController extends Controller
 {
@@ -24,6 +25,7 @@ class HomeController extends Controller
     public function index(Request $request)
     {
         $success = $request->success;
+        
         return view('home', compact('success'));
     }
 }
